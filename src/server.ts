5d0489d8a72a1b1e354fe3682ad75eb57e7e7e48
Nikola Balkov <balkov_94@abv.bg>
2023-10-27 19:14:52 +0300

    change local host with ap num to fix mongodb err when use mongodb as windows service

--- src/server.ts
+++ src/server.ts
@@ -14,11 +14,11 @@ import { MongoClient } from 'mongodb';
 export const HOSTNAME = 'localhost';
 export const PORT = 8000;
 
-//const dbUrl = `mongodb://localhost:27017`;
+//const dbUrl = `mongodb://localhost:27017`; NOT working need IP v6 / localhost!==IP v6
+const dbUrl = `mongodb://127.0.0.1:27017`;
 // got problen with configuration on mongodb as a service on (new work machine)
 // 27.10.23 done some additional registration and configs now DB is migrated to Atlas (free 512mg cluster - ClusterO)
-
-const dbUrl = `mongodb+srv://balkov:8393356@cluster0.suzbtho.mongodb.net/?retryWrites=true&w=majority`;
+//const dbUrl = `mongodb+srv://balkov:8393356@cluster0.suzbtho.mongodb.net/?retryWrites=true&w=majority`;
 const database = 'GoodBook';
 
 const app = express();
